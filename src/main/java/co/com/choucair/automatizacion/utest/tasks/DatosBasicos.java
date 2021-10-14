81e6be7708429608a3aafeff5c17b909933a0661
Lewis <ing.lewisvargasflorez@outlook.com>
2021-10-14 11:18:12 -0500

    Actualizando Reto Tecnico

--- src/main/java/co/com/choucair/automatizacion/utest/tasks/DatosBasicos.java
+++ src/main/java/co/com/choucair/automatizacion/utest/tasks/DatosBasicos.java
@@ -9,17 +9,29 @@ import net.serenitybdd.screenplay.actions.Enter;
 
 public class DatosBasicos implements Task {
 
-    public static DatosBasicos onThePage() {
-        return Tasks.instrumented(DatosBasicos.class);
+    private String strName;
+    private String strLastName;
+    private String strEmail;
+    private String strDate;
+
+    public DatosBasicos(String strName, String strLastName, String strEmail, String strDate) {
+        this.strName = strName;
+        this.strLastName = strLastName;
+        this.strEmail = strEmail;
+        this.strDate = strDate;
+    }
+
+    public static DatosBasicos onThePage(String strName, String strLastName, String strEmail, String strDate) {
+        return Tasks.instrumented(DatosBasicos.class, strName, strLastName, strEmail, strDate);
     }
 
     @Override
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(Click.on(DatosBasicosPage.REGISTER_BUTTON),
-                Enter.theValue("IngresarNombre").into(DatosBasicosPage.INPUT_NAME),
-                Enter.theValue("IngresarApellidos").into(DatosBasicosPage.INPUT_LAST_NAME),
-                Enter.theValue("IngresarCorreo").into(DatosBasicosPage.INPUT_EMAIL),
-                Enter.theValue("SeleccionarFechaN").into(DatosBasicosPage.SELECT_DATE),
+                Enter.theValue(strName).into(DatosBasicosPage.INPUT_NAME),
+                Enter.theValue(strLastName).into(DatosBasicosPage.INPUT_LAST_NAME),
+                Enter.theValue(strEmail).into(DatosBasicosPage.INPUT_EMAIL),
+                Enter.theValue(strDate).into(DatosBasicosPage.SELECT_DATE),
                 Click.on(DatosBasicosPage.NEXT_BUTTON_LOCATION)
         );
     }
